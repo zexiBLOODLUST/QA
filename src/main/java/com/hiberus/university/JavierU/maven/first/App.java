f0bd518fb55eafd71b18593a042c967a6705016a
zexiBLOODLUST <javieruriz1989@gmail.com>
2022-04-22 13:05:03 +0200

    y mas cambios

--- src/main/java/com/hiberus/university/JavierU/maven/first/App.java
+++ src/main/java/com/hiberus/university/JavierU/maven/first/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hola compañeros del QA!" );
+        System.out.println( "¡Hola compañeros del QA, estudiad mucho!" );
     }
 }
